fab6adc4191fb6f43a6898d588326f0c693cde94
Reyem Elyk <KyleMeyer@reyems-macbook-pro.local>
2017-12-28 12:25:51 -0700

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Reyem Elyk on 2017-12-28.
+//  Copyright © 2017 Reyem Elyk. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
